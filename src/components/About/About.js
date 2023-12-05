2224271c4c906f2bf96be1a52a006ec41356eac0
melegithubyit <Melakeselamyitbarek2012@gmail.com>
2023-12-05 13:51:52 +0300

    adding loading component for the video

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,34 +9,31 @@ import Card from "./Card";
 import { useMediaQuery } from "react-responsive";
 import deco from "../../assets/images/vision-and-mission/deco-line.svg";
 
-
-   const doc1 = {
-     title: "Innovation",
-     paragraph:
-       "We are dedicated to fostering innovation, continuously seeking new and creative ",
-     image: Innovation,
-   };
-   const doc2 = {
-     title: "Innovation",
-     paragraph:
-       "We are dedicated to fostering innovation, continuously seeking new and creative ",
-     image: Innovation,
-   };
-   const doc3 = {
-     title: "Innovation",
-     paragraph:
-       "We are dedicated to fostering innovation, continuously seeking new and creative ",
-     image: Innovation,
-   }
-
+const doc1 = {
+  title: "Innovation",
+  paragraph:
+    "We are dedicated to fostering innovation, continuously seeking new and creative ",
+  image: Innovation,
+};
+const doc2 = {
+  title: "Innovation",
+  paragraph:
+    "We are dedicated to fostering innovation, continuously seeking new and creative ",
+  image: Innovation,
+};
+const doc3 = {
+  title: "Innovation",
+  paragraph:
+    "We are dedicated to fostering innovation, continuously seeking new and creative ",
+  image: Innovation,
+};
 
 const About = () => {
-
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const xValue = isMobile ? 50 : 200;
 
   return (
-    <div className="md:mb-20 mb-4 pt-12"  id = "about">
+    <div className="md:mb-20 mb-4 pt-12" id="about">
       <div className="md:py-12 md:grid md:grid-cols-3 md:gap-20 overflow-hidden">
         <motion.h2
           className="md:text-5xl text-4xl font-extrabold text-[#00407B] md:col-span-1"
@@ -66,9 +63,8 @@ const About = () => {
           visible: { opacity: 1, x: 0 },
         }}
       >
-      
-        <div className="text-left flex-row mr-4 p-4 text-2xl text-left sm:text-[25px] mb-4 md:mb-12 text-gray-900 md:ml-[200px] ml-6">
-        <img src={deco} />
+        <div className="flex-row mr-4 p-4 text-2xl text-left sm:text-[25px] mb-4 md:mb-12 text-gray-900 md:ml-[200px] ml-6">
+          <img src={deco} />
           <h2 className="">Know the Business</h2>
           <p className="text-gray-900 font-bold">We are Committed to</p>
         </div>
@@ -79,7 +75,7 @@ const About = () => {
           initial="hidden"
           whileInView="visible"
           // viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 1}}
+          transition={{ duration: 1 }}
           variants={{
             hidden: { opacity: 0, y: 100 },
             visible: { opacity: 1, y: 0 },
@@ -113,7 +109,7 @@ const About = () => {
           initial="hidden"
           whileInView="visible"
           // viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 1, delay: 0.3}}
+          transition={{ duration: 1, delay: 0.3 }}
           variants={{
             hidden: { opacity: 0, y: 100 },
             visible: { opacity: 1, y: 0 },
--- src/components/home/home_page.jsx
+++ src/components/home/home_page.jsx
@@ -1,173 +1,48 @@
-
 import React, { useState, useEffect } from 'react';
 import videoAsset from "../../assets/images/home/vid.mp4";
+import LoadingComponent from './LoadingComponent';
 
 const HomePage = () => {
-  // const [player, setPlayer] = useState(null);
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
-  // useEffect(() => {
-  //   if (player) {
-  //     player.playVideo();
-  //     player.fullscreen();
-  //   }
-  // }, [player]);
+  useEffect(() => {
+    const handleVideoLoad = () => {
+      setVideoLoaded(true);
+    };
 
-  // const onReady = (event) => {
-  //   setPlayer(event.target);
-  // };
-  
+    const videoElement = document.querySelector('.video-element');
+    videoElement.addEventListener('loadeddata', handleVideoLoad);
+
+    return () => {
+      videoElement.removeEventListener('loadeddata', handleVideoLoad);
+    };
+  }, []);
+
   return (
     <div className="relative min-h-screen">
-    
-    <video
-      autoPlay
-      muted
-      loop
-      playbackRate={0.5}
-      className="absolute inset-0  object-cover w-full h-full"
-    >
-      <source
-        src={videoAsset}
-        type="video/mp4"
-      />
-      Your browser does not support the video tag.
-    </video> 
-    <div className="absolute inset-0 bg-black opacity-10"></div>
-    <div className="grid grid-cols-1 md:grid-cols-2 text-secondary-text  pt-16 mt- flex sjustify-center">
-     </div>
-    
-   </div>
+      {!videoLoaded && (
+        <div className="loading-component">
+          <LoadingComponent/>
+        </div>
+      )}
 
-    // <div className="relative min-h-screen">
-    //   <div className = "mt-24"></div>
-    //   <YouTube
-    //     videoId={`mnT18gWDpqI?si=TiYDACe9ibFDDpFI`}
-    //     opts={{ playerVars: { autoplay: 1, modestbranding: 1, controls: 0 , } }}
-    //     onReady={onReady}
-    //   />
-    // </div>
-  //    <div className="relative min-h-screen">
-  //    <div className="mt-24"></div>
-  //    <YouTube
-  //      videoId={`mnT18gWDpqI?si=TiYDACe9ibFDDpFI`}
-  //      opts={{
-  //        playerVars: {
-  //          autoplay: 1,
-  //          modestbranding: 1,
-  //          controls: 0,
-  //        },
-  //      }}
-  //      onReady={onReady}
-  //    />
-  //  </div>
-    // <div className="relative min-h-screen">
-    //   <ReactPlayer
-    //         url={`https://www.youtube.com/embed/yY13Y9a3Naw?si=zllhESflJ389kGs-?fs=1"`}
-    //         playing={true}
-    //         muted = {true}
-    //         loop = {true}
-    //         width={window.innerWidth}
-    //         height={window.innerHeight}
-    //         config={{
-    //           youtube: {
-    //             playerVars: {
-    //               autoplay: 1,
-    //               modestbranding: 1,
-    //               controls: 0,
-    //               showinfo: 0,
-    //               mute: 1,
-    //               rel: 0,
-    //               fs: 1, // Enabling fullscreen
-    //               hd: 1
-    //             },
-    //           },
-    //         }}
-    //       />
-          //  <iframe
-          //   title="YouTube Live Stream"
-          //   width="100%"
-          //   height="100%"
-          //   src="https://www.youtube.com/embed/yY13Y9a3Naw?si=zllhESflJ389kGs-?autoplay=1&mute=1&modestbranding=1&controls=0&showinfo=0&mute=1&rel=0"
-          //   muted
-          //   allow="autoplay; encrypted-media"
-          //   allowFullScreen
-          //   className="absolute inset-0"
-          // ></iframe> 
-          //  <div className="absolute inset-0 bg-black opacity-10"></div> 
-        // </div>
-      
-    //  <div className="grid grid-cols-1 md:grid-cols-2 text-secondary-text  pt-16 mt- flex sjustify-center">
-    //  </div>
-    
+      <video
+        autoPlay
+        muted
+        loop
+        playbackRate={0.5}
+        className="absolute inset-0 object-cover w-full h-full video-element"
+      >
+        <source src={videoAsset} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-   
+      <div className="absolute inset-0 bg-black opacity-10"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 text-secondary-text  pt-16 md:flex justify-center">
+        {/* Add your content here */}
+      </div>
+    </div>
   );
 };
 
 export default HomePage;
-
-// import React from "react";
-// import exportImage from "../../assets/images/home/export.png";
-// import ell185 from "../../assets/images/home/Ellipse-185.svg";
-// import ell180 from "../../assets/images/home/Ellipse-180.svg";
-// import ell190 from "../../assets/images/home/Ellipse-190.svg";
-// import ell191 from "../../assets/images/home/Ellipse-191.svg";
-// import ell193 from "../../assets/images/home/Ellipse-193.svg";
-// import logo from "../../assets/images/home/logo.png";
-// import videoAsset from "../../assets/images/home/vid.mp4";
-
-// const HomePage = () => {
-//   return (
-//     <div className="relative min-h-screen">
-//       <video
-//         autoPlay
-//         muted
-//         loop
-//         playsInline
-//         playbackRate={0.5}
-//         className="absolute inset-0 object-cover w-full h-full"
-//       >
-//         <source src={videoAsset} type="video/mp4" />
-//         Your browser does not support the video tag.
-//       </video>
-//       <div className="absolute inset-0 bg-black opacity-10"></div>
-
-//       {/* Card Overlay */}
-//       <div className="relative z-10 pt-96 grid grid-cols-1 md:grid-cols-3 text-secondary-text mx-4 md:mx-36 flex flex-col justify-end">
-
-//         <div className="md:col-span-1 p-8 md:p-16 ">
-//         <p className="text-lg md:text-xl text-white">
-//             Inspiring
-//           </p>
-//           <h2 className="text-4xl md:text-5xl lg:text-6xl text-white font-bold mb-4">
-//             Innovation
-//           </h2>
-         
-//         </div>
-
-//         <div className="md:col-span-1 p-8 md:p-16 flex flex-col justify-end">
-//         <p className="text-lg md:text-xl text-white">
-//             Driving
-//           </p>
-//           <h2 className="text-4xl md:text-5xl lg:text-6xl text-white font-bold mb-4">
-//             Investment
-//           </h2>
-         
-//         </div>
-//         <div className="md:col-span-1 p-8 md:p-16 flex flex-col justify-end">
-//         <p className="text-lg md:text-xl text-white">
-//             Empowering
-//           </p>
-//           <h2 className="text-4xl md:text-5xl lg:text-6xl text-white font-bold mb-4">
-//             Growth
-//           </h2>
-          
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
-
