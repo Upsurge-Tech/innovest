9b16b0925f4e545312bd19775ff08c0f22bb5d42
mekdeskebede <kebedemekdes289@gmail.com>
2023-12-11 09:24:39 +0300

    populate data

--- src/components/new_service/Document.js
+++ src/components/new_service/Document.js
@@ -1,6 +1,6 @@
-import Coffee1 from '../../assets/images/services/cofee.jpg';
+import Coffee1 from "../../assets/images/services/cofee.jpg";
 import Coffee2 from "../../assets/images/services/cofeeexp.jpeg";
-import Coffee3  from "../../assets/images/services/cofe.jpg";
+import Coffee3 from "../../assets/images/services/cofe.jpg";
 import fork1 from "../../assets/images/services/const.jpg";
 import fork2 from "../../assets/images/services/machinery.webp";
 import fork3 from "../../assets/images/services/mach.jpg";
@@ -11,15 +11,15 @@ import buil3 from "../../assets/images/services/elvate.jpg";
 import topaz1 from "../../assets/images/services/coal.jpg";
 import topaz2 from "../../assets/images/services/white-rocks.jpg";
 import topaz3 from "../../assets/images/services/red-salt.jpg";
-
-
+import elevator1 from "../../assets/images/services/elevator1.jpg";
+import elevator2 from "../../assets/images/services/elevator2.PNG";
+import elevator3 from "../../assets/images/services/elevator3.PNG";
 
 export const Coffee_list = [Coffee1, Coffee2, Coffee3];
-export const Building_list = [buil1,buil2,buil3];
-export const Curtains_list = [Coffee1, Coffee2, Coffee3];
+export const Building_list = [buil1, buil2, buil3];
+export const Curtains_list = [elevator1, elevator2, elevator3];
 export const Forklift_list = [fork1, fork2, fork3];
 export const Topaz_list = [topaz1, topaz2, topaz3];
-
 
 export const para_topaz = {
   title:
@@ -34,46 +34,43 @@ export const para_topaz = {
 
 export const para_Building = {
   title:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam aspernatur itaque cumque aliquid atque.",
+    "Discover innovative real estate solutions tailored to your needs. From residential to commercial properties.",
   para1:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam aspernatur itaque cumque aliquid atque. Esse repellat dicta inventore consectetur, nulla pariatur, minus corrupti quidem perspiciatis obcaecati neque!",
+    " Experience the future of real estate with our innovative solutions tailored to redefine the industry. We specialize in crafting forward-thinking approaches that elevate traditional real estate models. we are committed to transforming the real estate landscape.",
   para2:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam aspernatur itaque cumque aliquid atque. Esse repellat dicta inventore consectetur, nulla pariatur, minus corrupti quidem perspiciatis obcaecati neque!",
+    "Partner with us to embrace innovation and stay ahead in a dynamic market. Step into a new era of real estate with our revolutionary solutions designed to disrupt the conventional paradigm.",
   para3:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam aspernatur itaque cumque aliquid atque. Esse repellat dicta inventore consectetur, nulla pariatur, minus corrupti quidem perspiciatis obcaecati neque!",
+    "As your dedicated partner in real estate, we offer more than just solutions; we offer a vision for tomorrow's real estate landscape. Our approach combines creativity, sustainability, and technological prowess.",
 };
 
 export const para_Coffee = {
   title:
-    "Our expertise in mineral export includes industrial and construction minerals. We excel in quality control,",
+    "Our proficiency in coffee bean export encompasses premium-grade coffee beans. You can rely on us as your trusted partner in the coffee industry.",
   para1:
-    "Our expertise in mineral export includes industrial and construction minerals. We excel in quality control, reliable supply, and adherence to international standards, ensuring you can trust us as your partner in this sector.",
+    "Elevate your coffee experience with our premium-grade coffee beans. We take pride in exporting only the finest beans, carefully selected and cultivated for an unparalleled richness of flavor. Our commitment to excellence is reflected in every batch we export.",
   para2:
-    "Our team possesses an in-depth understanding of the nuances of mineral export. We have honed our expertise over the years, allowing us to navigate the complexities of international markets and supply chains with confidence.",
+    "At the core of our coffee bean export operation is a commitment to dependable supply and unwavering quality. We understand the importance of a reliable source in the coffee industry. ",
   para3:
-    "Our team possesses an in-depth understanding of the nuances of mineral export. We have honed our expertise over the years, allowing us to navigate the complexities of international markets and supply chains with confidence.",
+    " Our adherence to international standards ensures that our beans not only meet regulations but also surpass expectations. Whether you're a coffee roaster,distributor, or retailer, consider us your dedicated ally in the coffee business.",
 };
-
 export const para_Curtains = {
   title:
-    "Our expertise in mineral export includes industrial and construction minerals. We excel in quality control,",
+    "Discover our selection of imported construction elevators renowned for their superior quality.",
   para1:
-    "Our expertise in mineral export includes industrial and construction minerals. We excel in quality control, reliable supply, and adherence to international standards, ensuring you can trust us as your partner in this sector.",
+    "Our focus is on ensuring reliability, efficiency, and adherence to top-notch standards, guaranteeing seamless operations for your services and projects. Rely on us for the procurement of premium equipment, and experience the difference in performance.",
   para2:
-    "Our team possesses an in-depth understanding of the nuances of mineral export. We have honed our expertise over the years, allowing us to navigate the complexities of international markets and supply chains with confidence.",
+    "Embark on a revolutionary journey in the elevator industry with our forward-looking solutions designed to reshape the landscape. We excel in developing visionary approaches that transcend conventional elevator models. From state-of-the-art technological advancements to sustainable design innovations.",
   para3:
-    "Our team possesses an in-depth understanding of the nuances of mineral export. We have honed our expertise over the years, allowing us to navigate the complexities of international markets and supply chains with confidence.",
+    "Step into a new era of importing Elevators with our revolutionary solutions designed to disrupt the conventional paradigm. Our commitment to innovation extends across the entire spectrum.",
 };
 
 export const para_Forklift = {
   title:
-    "Our expertise in mineral export includes industrial and construction minerals. We excel in quality control,",
+    "Explore our range of high-quality construction machinery imports. We prioritize reliability, efficiency, and top-notch standards.",
   para1:
-    "Our expertise in mineral export includes industrial and construction minerals. We excel in quality control, reliable supply, and adherence to international standards, ensuring you can trust us as your partner in this sector.",
+    "Transform your construction projects with our high-quality machinery imports. We specialize in sourcing and delivering top-tier construction equipment that meets the highest industry standards. ",
   para2:
-    "Our team possesses an in-depth understanding of the nuances of mineral export. We have honed our expertise over the years, allowing us to navigate the complexities of international markets and supply chains with confidence.",
+    " Depend on us for a steady supply of construction machinery that redefines quality. Our import operations prioritize the consistent delivery of top-notch equipment, backed by stringent quality control measures.",
   para3:
-    "Our team possesses an in-depth understanding of the nuances of mineral export. We have honed our expertise over the years, allowing us to navigate the complexities of international markets and supply chains with confidence.",
+    " As a trusted ally in the construction industry, we offer more than just machinery imports – we offer a commitment to your success. Our adherence to international standards ensures that each piece of equipment surpasses regulatory requirements.",
 };
-
-
